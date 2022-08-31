db2dc757868013bac72005c7e6791d25f33c3cb4
buudee625 <liu.andrew625@gmail.com>
2022-08-31 16:21:21 -0700

    adds 2nd model Review

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Guitar
+from .models import Guitar, Review
 admin.site.register(Guitar)
+admin.site.register(Review)
--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
 class Guitar(models.Model):
         
     KIND_CHOICES = (
@@ -20,7 +22,10 @@ class Guitar(models.Model):
     brand = models.CharField(max_length=50)
     kind = models.CharField(max_length=2, choices=KIND_CHOICES)
     model = models.CharField(max_length=50)
-    string = models.IntegerField(default=6)
+    string = models.PositiveIntegerField(default=6, validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ])
     released = models.BooleanField()
 
     def get_absolute_url(self):
@@ -28,3 +33,13 @@ class Guitar(models.Model):
 
     def __str__(self):
         return f'{self.brand} - {self.model}'
+
+
+class Review(models.Model):
+    date = models.DateField(auto_now_add=True)
+    rating = models.PositiveIntegerField(default=5, validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
+    review = models.TextField(max_length=300)
+    guitar = models.ForeignKey(Guitar, on_delete=models.CASCADE)
